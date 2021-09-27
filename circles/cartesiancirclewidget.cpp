34a720725c18fe370b3e78546a87f7c15876b7d2
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2021-09-27 12:30:17 +0530

    Use corrected version of Cartesian circle drawing

--- circles/cartesiancirclewidget.cpp
+++ circles/cartesiancirclewidget.cpp
@@ -9,10 +9,16 @@ const QList<QPoint> CartesianCircleWidget::drawCircle()
 {
     QList<QPoint> points;
     points.append(QPoint(cx, cy));
-    for (qreal x = -r, y = 0; x <= r; x++, y = std::sqrt(1.0 * r * r - x * x))
+    qreal t = r / std::sqrt(2);
+    for (qreal x = -t, y = t; x <= t; x++, y = std::sqrt(1.0 * r * r - x * x))
     {
         points.append(QPoint(std::round(cx + x), std::round(cy + y)));
         points.append(QPoint(std::round(cx + x), std::round(cy - y)));
+    }
+    for (qreal y = -t, x = t; y <= t; y++, x = std::sqrt(1.0 * r * r - y * y))
+    {
+        points.append(QPoint(std::round(cx + x), std::round(cy + y)));
+        points.append(QPoint(std::round(cx - x), std::round(cy + y)));
     }
     return points;
 }
